91afa8f398ce2cad4737953afab811145ed436ae
Breno Medeiros de Oliveira <breno.medeiros@icloud.com>
2020-03-29 13:29:42 -0300

    OpenCV Basics - 04 - Accessing Pixels using at Method
    We familiarize ourselves with the Mat.at method for accessing individual pixels by row and column. We look at both gray scale and color images.

--- BMOOpenCV-04/main.cpp
+++ BMOOpenCV-04/main.cpp
@@ -6,10 +6,36 @@
 //  Copyright © 2020 ProgramasBMO. All rights reserved.
 //
 
-#include <iostream>
+#include <opencv2/opencv.hpp>
+#include "stdint.h"
+
+using namespace cv;
+using namespace std;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    String caminho = "/Users/brenomedeiros/Documents/ProgramasBMO/Cpp/BMOOpenCV-04/BMOOpenCV-04/";
+    String caminhoImagens = "/Users/brenomedeiros/Documents/ProgramasBMO/Cpp/BMOOpenCV-04/BMOOpenCV-04/Images/";
+    
+    Mat original = imread(caminhoImagens+"Me.jpeg", CV_LOAD_IMAGE_COLOR);
+    Mat cinzaEscuro = imread(caminhoImagens+"Me.jpeg", CV_LOAD_IMAGE_GRAYSCALE);
+    Mat coloridoEscuro = imread(caminhoImagens+"Me.jpeg", CV_LOAD_IMAGE_COLOR);
+    
+    for (int linha=0; linha<coloridoEscuro.rows; linha++) {
+        for (int coluna=0; coluna<coloridoEscuro.cols; coluna++) {
+            cinzaEscuro.at<uint8_t>(linha, coluna) = cinzaEscuro.at<uint8_t>(linha,coluna) * 0.5f; //Azul
+            cinzaEscuro.at<uint8_t>(linha, coluna) = cinzaEscuro.at<uint8_t>(linha,coluna) * 0.5f; //Verde
+            coloridoEscuro.at<cv::Vec3b>(linha, coluna)[0] = coloridoEscuro.at<cv::Vec3b>(linha,coluna)[0] * 0.5f; //Azul
+            coloridoEscuro.at<cv::Vec3b>(linha, coluna)[1] = coloridoEscuro.at<cv::Vec3b>(linha,coluna)[1] * 0.5f; //Verde
+            coloridoEscuro.at<cv::Vec3b>(linha, coluna)[2] = coloridoEscuro.at<cv::Vec3b>(linha,coluna)[2] * 0.5f; //Vermelho
+        }
+    }
+    
+    imshow("Original", original);
+    imshow("Cinza Escuro", cinzaEscuro);
+    imshow("Colorido Escuro", coloridoEscuro);
+    
+    waitKey();
+
     return 0;
 }
